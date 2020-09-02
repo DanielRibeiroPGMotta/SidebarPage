315d9caead2403c83ac227a2d90093f7c3d1af0d
Daniel Ribeiro P.G. Motta <43221706+DanielRibeiroPGMotta@users.noreply.github.com>
2020-09-02 10:55:57 -0300

    Add files via upload

---
+++ src/pages/Products.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../components/Navbar.css';
+
+export default function Products()  {
+    return (
+        
+            <h1>Seus Produtos</h1>
+          
+       
+    )
+}
---
+++ src/pages/Reports.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Reports() {
+    return (
+        <div>
+            <h1>Seus Reports</h1>
+        </div>
+    )
+}
